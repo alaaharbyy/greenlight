ae6147d9640fd1a35e2a1c7f5468b61d26df9dfc
alaa harby <alaa@aqaryint.com>
2025-03-26 14:20:18 +0400

    :zap: error handling, logging

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -15,7 +15,7 @@ func (app *application) creatMovieHandler(w http.ResponseWriter, r *http.Request
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
@@ -31,8 +31,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	err = app.writeJSON(w, http.StatusOK, movie, nil)
 	if err != nil {
-		app.logger.Print(err)
-		http.Error(w, "the server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.servereErrorResponse(w, r, err)
 		return
 	}
 }
